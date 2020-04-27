eef31731bb059fa9e053c3db0ca312a0cbbc91a6
tejpk <43774465+tejpk@users.noreply.github.com>
2020-04-27 13:45:10 +0530

    Create DP_Longest_Increasing_Subsequence.py

---
+++ DP_Longest_Increasing_Subsequence.py
@@ -0,0 +1,15 @@
+def lis(arr): 
+    n = len(arr)
+    lis = [1]*n 
+    for i in range (1 , n): 
+        for j in range(0 , i): 
+            if arr[i] > arr[j] and lis[i]< lis[j] + 1 : 
+                lis[i] = lis[j]+1
+    maximum = 0
+    for i in range(n): 
+        maximum = max(maximum , lis[i]) 
+  
+    return maximum 
+    
+arr = list(map(int,input().split()))
+print(lis(arr))
