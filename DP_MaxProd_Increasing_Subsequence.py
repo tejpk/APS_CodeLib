0c31d092ca480f2464281e7832232a916a4fce46
tejpk <43774465+tejpk@users.noreply.github.com>
2020-04-27 14:20:17 +0530

    Create DP_MaxProd_Increasing_Subsequence.py

---
+++ DP_MaxProd_Increasing_Subsequence.py
@@ -0,0 +1,14 @@
+def lis (arr, n ): 
+    mpis =[0] * (n) 
+    for i in range(n): 
+        mpis[i] = arr[i] 
+    for i in range(1, n): 
+        for j in range(i): 
+            if (arr[i] > arr[j] and
+                    mpis[i] < (mpis[j] * arr[i])): 
+                        mpis[i] = mpis[j] * arr[i] 
+    return max(mpis) 
+arr= list(map(int,input().split()))
+n = len(arr) 
+print( lis(arr, n)) 
+  
