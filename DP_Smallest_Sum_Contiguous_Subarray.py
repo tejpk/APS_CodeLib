25cc44bd38599fd8e2e232734fa716befc3cb29c
tejpk <43774465+tejpk@users.noreply.github.com>
2020-05-04 23:09:52 +0530

    Create DP_Smallest_Sum_Contiguous_Subarray.py

---
+++ DP_Smallest_Sum_Contiguous_Subarray.py
@@ -0,0 +1,14 @@
+import sys 
+def smallestSumSubarr(arr, n):
+    min_ending_here = sys.maxsize 
+    min_so_far = sys.maxsize 
+    for i in range(n): 
+        if (min_ending_here > 0): 
+            min_ending_here = arr[i]
+        else: 
+            min_ending_here += arr[i]
+        min_so_far = min(min_so_far, min_ending_here) 
+    return min_so_far 
+arr =list(map(int,input().split()))
+n = len(arr) 
+print "Smallest sum: ", smallestSumSubarr(arr, n) 
