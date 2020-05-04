a50be107d383ee8ce99822d8314a3bf9814edaca
tejpk <43774465+tejpk@users.noreply.github.com>
2020-05-05 00:01:46 +0530

    Create CC_FSQRT.cpp

---
+++ CC_FSQRT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	long int n;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>n;
+	    cout<<round(sqrt(n))<<endl;
+	    
+	}
+	return 0;
+}
