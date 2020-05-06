34a3eafc44759cf3ce0c2b7b24edcd8b9f41fd1c
tejpk <43774465+tejpk@users.noreply.github.com>
2020-05-06 20:58:29 +0530

    Create CC_PINS.cpp

---
+++ CC_PINS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int t;
+  unsigned int n;
+   cin>>t;
+   while(t--)
+   {
+       long int q=1,count=0;
+       cin>>n;
+       count=n/2;
+       cout<<"1"<<" "<<1;
+       for(int i=0;i<count;i++)
+       cout<<"0";
+        cout<<endl;
+     
+   }
+  
+    return 0;
+} 
