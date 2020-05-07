2993c16018a17734e0e0a6cf203b3c66e0b0a9fc
tejpk <43774465+tejpk@users.noreply.github.com>
2020-05-07 22:22:46 +0530

    Create Upper_To_Lower.cpp

---
+++ Upper_To_Lower.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++)
+    {
+        s[i]=s[i]|32;
+    }
+
+    cout<<s<<endl;
+}
+© 2020 GitHub, Inc.
